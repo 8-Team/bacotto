7fef5d9f4434c226af6ee7d2099750d720303861
Pietro Lorefice <pietro@develer.com>
2017-11-08 11:09:46 +0100

    api: extract authorization API

--- api/api.go
+++ api/api.go
@@ -4,30 +4,20 @@ import (
 	"net/http"
 
 	"github.com/8-team/bacotto/conf"
-	"github.com/8-team/bacotto/db"
 	"github.com/Sirupsen/logrus"
-	"github.com/pquerna/otp/totp"
 )
 
 var log = logrus.WithField("app", "api")
 
 func pong(w http.ResponseWriter, r *http.Request) {
-	var otto db.Otto
-
 	serial := r.FormValue("serial")
 	otp := r.FormValue("otp")
 
-	if err := db.DB.First(&otto, "serial = ?", serial).Error; err != nil {
-		log.Errorf("http request from invalid Otto serial %s: %s", serial, err)
+	if err := Authorize(serial, otp); err != nil {
+		log.Errorln(err)
 		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	if totp.Validate(otp, otto.OTPSecret) {
-		w.WriteHeader(http.StatusOK)
 	} else {
-		log.Warningf("invalid OTP code %s from %s", otp, serial)
-		w.WriteHeader(http.StatusUnauthorized)
+		w.WriteHeader(http.StatusOK)
 	}
 }
 
---
+++ api/auth.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/8-team/bacotto/db"
+	"github.com/pquerna/otp/totp"
+)
+
+// Authorize authenticates a request from an Otto by using the provided OTP code.
+func Authorize(serial string, otp string) error {
+	var otto db.Otto
+
+	if err := db.DB.First(&otto, "serial = ?", serial).Error; err != nil {
+		return fmt.Errorf("http request from invalid Otto serial %s: %s", serial, err)
+	}
+
+	if !totp.Validate(otp, otto.OTPSecret) {
+		return fmt.Errorf("invalid OTP code %s from %s", otp, serial)
+	}
+
+	return nil
+}
