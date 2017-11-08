2baa996c8c27fe75a4a732ac4ff8b3a699a0ce1a
Pietro Lorefice <pietro@develer.com>
2017-11-08 11:03:51 +0100

    api: add ping endpoint

--- api/api.go
+++ api/api.go
@@ -4,11 +4,31 @@ import (
 	"net/http"
 
 	"github.com/8-team/bacotto/conf"
+	"github.com/8-team/bacotto/db"
+	"github.com/Sirupsen/logrus"
+	"github.com/pquerna/otp/totp"
 )
 
+var log = logrus.WithField("app", "api")
+
 func pong(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("HTTPS pong"))
+	var otto db.Otto
+
+	serial := r.FormValue("serial")
+	otp := r.FormValue("otp")
+
+	if err := db.DB.First(&otto, "serial = ?", serial).Error; err != nil {
+		log.Errorf("http request from invalid Otto serial %s: %s", serial, err)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if totp.Validate(otp, otto.OTPSecret) {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		log.Warningf("invalid OTP code %s from %s", otp, serial)
+		w.WriteHeader(http.StatusUnauthorized)
+	}
 }
 
 func ListenAndServe(addr string) error {
