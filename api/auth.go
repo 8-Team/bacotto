3078f0feca11a705149a09bc0fd03410907a81bf
Daniele Basile <asterix24@gmail.com>
2018-02-01 14:31:15 +0100

    Bacotto: return User oject after auth for other operation.

--- api/auth.go
+++ api/auth.go
@@ -9,20 +9,21 @@ import (
 )
 
 // Authorize authenticates a request from an Otto by using the provided OTP code.
-func Authorize(serial string, otp string) error {
+func Authorize(serial string, otp string) (db.Otto, error) {
 	var otto db.Otto
+
+	if err := db.DB.First(&otto, "serial = ?", serial).Error; err != nil {
+		return otto, fmt.Errorf("http request from invalid Otto serial %s: %s", serial, err)
+	}
 
 	if !conf.VerifyMsg() {
 		log.Warn("You are in debug mode, no serial and OPT validation! check config file.")
-		return nil
-	}
-
-	if err := db.DB.First(&otto, "serial = ?", serial).Error; err != nil {
-		return fmt.Errorf("http request from invalid Otto serial %s: %s", serial, err)
+		return otto, nil
 	}
 
 	if !totp.Validate(otp, otto.OTPSecret) {
-		return fmt.Errorf("invalid OTP code %s from %s", otp, serial)
+		return otto, fmt.Errorf("invalid OTP code %s from %s", otp, serial)
 	}
-	return nil
+
+	return otto, nil
 }
