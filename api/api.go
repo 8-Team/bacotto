d5d6a38aeed178daca1d9003e07d2d76dd9d1997
Daniele Basile <asterix24@gmail.com>
2018-02-01 14:32:14 +0100

    Bacotto: start to implement entry registration.

--- api/api.go
+++ api/api.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/8-team/bacotto/conf"
 	"github.com/8-team/bacotto/db"
@@ -17,7 +18,7 @@ func isAuthorize(r *http.Request) bool {
 	serial := r.FormValue("serial")
 	otp := r.FormValue("otp")
 
-	if err := Authorize(serial, otp); err != nil {
+	if _, err := Authorize(serial, otp); err != nil {
 		log.Errorln(err)
 		return false
 	}
@@ -32,7 +33,7 @@ func pong(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func prjlist(w http.ResponseWriter, r *http.Request) {
+func prjList(w http.ResponseWriter, r *http.Request) {
 	if !isAuthorize(r) {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
@@ -79,9 +80,67 @@ func prjlist(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
+type OttoProjectEntry struct {
+	ProjectID uint
+	StartDate time.Time
+	Duration  time.Duration
+	Serial    string
+	OTP       string
+}
+
+func registerEntry(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var t OttoProjectEntry
+	err := decoder.Decode(&t)
+	if err != nil {
+		log.Error(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+	log.Info(t.ProjectID, t.StartDate, t.Duration)
+
+	otto, err := Authorize(t.Serial, t.OTP)
+	if err != nil {
+		log.Errorln(err)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if otto.UserID == nil {
+		log.Error("No user registered for this Otto.")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// search project by id and add/update entry
+	prj := db.Project{}
+	if db.DB.Find(&prj, t.ProjectID).RecordNotFound() {
+		log.Error("Invalid Project ID: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Info(prj)
+
+	log.Info("User id", otto.UserID, otto.Serial)
+	entry := db.ProjectEntry{UserID: 0, ProjectID: t.ProjectID,
+		StartTime: t.StartDate,
+		EndTime:   t.StartDate.Add(t.Duration)}
+
+	if db.DB.NewRecord(rec) {
+		db.DB.Create(&rec)
+	}
+	if err := db.DB.Create(&entry, "project_id = ?", entry.ProjectID).Error; err != nil {
+		log.Error("Unable to add entry", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
 func ListenAndServe(addr string) error {
 	http.HandleFunc("/ping", pong)
-	http.HandleFunc("/prjlist", prjlist)
+	http.HandleFunc("/prjlist", prjList)
+	http.HandleFunc("/register", registerEntry)
 
 	if conf.UseHTTPS() {
 		return http.ListenAndServeTLS(addr,
