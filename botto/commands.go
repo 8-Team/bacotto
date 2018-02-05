34febb522207ddedb504138424638e1ca8c7c6a3
Pietro Lorefice <pietro.lorefice@gmail.com>
2018-02-05 19:56:38 +0100

    botto: minor text fixes

--- botto/commands.go
+++ botto/commands.go
@@ -7,7 +7,7 @@ import (
 func (uc *userContext) showHelp(bot *slackbot, ev contextEvent) {
 	text := `Here's some stuff you can do:
 ` + "`botto help` to show this help message" + `
-` + "`botto add project` to manage a project using your Otto"
+` + "`botto add project` to start managing a project using your Otto"
 
 	bot.Message(ev.channel(), text)
 }
